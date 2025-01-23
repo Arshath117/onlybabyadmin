1e67554d8e425a6d63609c001750368955e8709f
SanjeevikumarWD <sanjeevikumar585@gmail.com>
2025-01-23 07:07:30 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,9 @@ const app = express();
 const __dirname = path.resolve();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin:["http://localhost:5173","https://onlybaby-admin.onrender.com"],
+}));
 
 
 app.use("/api/products",productRouter);
