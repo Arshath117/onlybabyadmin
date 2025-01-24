e6ed8a625904522016e6f1b35d25fbc5dfbdb833
SanjeevikumarWD <sanjeevikumar585@gmail.com>
2025-01-24 08:42:54 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use("/api/",orderRoutes);
 const PORT = process.env.PORT || 5001;
 
 
-if(process.env.NODE_ENV === "production"){
+if(process.env.NODE_ENV === "development"){
     app.use(express.static(path.join(__dirname,"/frontend/dist")));
     app.get("*",(req,res)=>{
         res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
