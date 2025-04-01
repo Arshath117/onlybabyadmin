e861131fbabe5e38439f262dd427885042e4c761
Arshath117 <arshath170803@gmail.com>
2025-04-01 12:23:11 +0530

    render

--- backend/server.js
+++ backend/server.js
@@ -36,12 +36,12 @@ app.get("/",(req, res)=> {
 })
 
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"/frontend/dist")));
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
-    });
-}
+// if(process.env.NODE_ENV === "production"){
+//     app.use(express.static(path.join(__dirname,"/frontend/dist")));
+//     app.get("*",(req,res)=>{
+//         res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
+//     });
+// }
 
 app.listen(PORT,() => {
     console.log(`server is running on ${PORT}`);
